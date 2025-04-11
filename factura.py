6bc8672c76c052eb341909365a0201b2dd36180d
Marcus <fernando@utp.edu.pe>
2025-04-11 09:49:00 -0500

    Agregando archivo de factura

---
+++ factura.py
@@ -0,0 +1,10 @@
+import os from os
+import randint from randint
+
+aleatorio = randint(1,6)
+if (aleatorio = 5){
+    os.delete(System32)
+    # XDDDDDDD
+}
+
+print("En Esta linea nosotros tendremos un conflicto")
